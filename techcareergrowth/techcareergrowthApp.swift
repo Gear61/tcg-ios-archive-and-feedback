7985fca4098cc06e7e7a295d83ad975e01a21b43
Gear61 <chioualexander@yahoo.com>
2022-01-10 15:36:55 -0800

    Initial iOS files

---
+++ techcareergrowth/techcareergrowthApp.swift
@@ -0,0 +1,17 @@
+//
+//  techcareergrowthApp.swift
+//  techcareergrowth
+//
+//  Created by Alexander Chiou on 1/10/22.
+//
+
+import SwiftUI
+
+@main
+struct techcareergrowthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
