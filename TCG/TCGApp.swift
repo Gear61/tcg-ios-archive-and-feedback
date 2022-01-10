28b60a942f94db6e60ff62f9af055d87de7d13c7
Gear61 <chioualexander@yahoo.com>
2022-01-10 15:54:27 -0800

    Initial commit

---
+++ TCG/TCGApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCGApp.swift
+//  TCG
+//
+//  Created by Alexander Chiou on 1/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TCGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
