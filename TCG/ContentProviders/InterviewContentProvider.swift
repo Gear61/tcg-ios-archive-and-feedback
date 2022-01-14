6cca21446990eb91f04f0f17462c1e592e1b2899
Gear61 <chioualexander@yahoo.com>
2022-01-13 22:45:07 -0800

    Add interviewing lesson 2

--- TCG/ContentProviders/InterviewContentProvider.swift
+++ TCG/ContentProviders/InterviewContentProvider.swift
@@ -96,14 +96,60 @@ class InterviewContentProvider: ObservableObject {
         var questions = [Question]()
         questions.append(
             Question(
-                text: "Which of these are the core build block of data structures and algorithms problems?",
+                text: "True or false: You need to care a lot about your Big Tech interview, because passing it will drastically improve your life. The more prominent it is in your brain, the more likely you are to pass it.",
                 options: [
-                    Question.Option(id: "Graphs"),
-                    Question.Option(id: "Patterns"),
-                    Question.Option(id: "Code"),
-                    Question.Option(id: "Trees")
+                    Question.Option(id: "True"),
+                    Question.Option(id: "False")
                 ],
-                correctAnswer: "Patterns"
+                correctAnswer: "False"
+            )
+        )
+        
+        questions.append(
+            Question(
+                text: "How should you view interviews?",
+                options: [
+                    Question.Option(id: "Make or break opportunities"),
+                    Question.Option(id: "Lottery tickets to make a lot more money"),
+                    Question.Option(id: "Opportunities to hang out with some talented tech people"),
+                    Question.Option(id: "Big tests, similar to final exams in school")
+                ],
+                correctAnswer: "Opportunities to hang out with some talented tech people"
+            )
+        )
+        
+        questions.append(
+            Question(
+                text: "True or false: Your input is much more important than the output and results you achieve.",
+                options: [
+                    Question.Option(id: "True"),
+                    Question.Option(id: "False")
+                ],
+                correctAnswer: "True"
+            )
+        )
+        
+        questions.append(
+            Question(
+                text: "Why should you not view interviews as a test?",
+                options: [
+                    Question.Option(id: "Because they aren't"),
+                    Question.Option(id: "To reduce your anxiety and care less about the result"),
+                    Question.Option(id: "So they don't remind you of school"),
+                    Question.Option(id: "It takes too much mental capacity")
+                ],
+                correctAnswer: "To reduce your anxiety and care less about the result"
+            )
+        )
+        
+        questions.append(
+            Question(
+                text: "True or false: You shouldn't take any breaks when studying for interviews, because the relentless focus will help you learn the material better.",
+                options: [
+                    Question.Option(id: "True"),
+                    Question.Option(id: "False")
+                ],
+                correctAnswer: "False"
             )
         )
         
