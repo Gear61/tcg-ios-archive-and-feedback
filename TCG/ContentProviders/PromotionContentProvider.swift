e5370cd4a1ed21624b708240a0fd85d9cbf5a591
Gear61 <chioualexander@yahoo.com>
2022-02-07 14:46:47 -0800

    Add first promotion lesson

--- TCG/ContentProviders/PromotionContentProvider.swift
+++ TCG/ContentProviders/PromotionContentProvider.swift
@@ -20,20 +20,20 @@ class PromotionContentProvider: ObservableObject {
         var questions = [Question]()
         questions.append(
             Question(
-                text: "What separates the top senior engineers from everyone else?",
+                text: "What is promotion fundamentally about?",
                 options: [
-                    Question.Option(id: "Writing code the fastest"),
-                    Question.Option(id: "Being able to break down large, ambiguous problems"),
-                    Question.Option(id: "Working harder than everyone else"),
+                    Question.Option(id: "Output"),
+                    Question.Option(id: "Behavior"),
+                    Question.Option(id: "Playing politics"),
                     Question.Option(id: "All of the above")
                 ],
-                correctAnswer: "Being able to break down large, ambiguous problems"
+                correctAnswer: "Behavior"
             )
         )
         
         questions.append(
             Question(
-                text: "True or false: One of the reasons top tech companies are so strong is because they are so well organized. Every project is neatly defined, so when engineers take them on, they can just execute quickly and easily.",
+                text: "True or false: To get to very senior engineering levels, you need to write thousands of lines of code per day.",
                 options: [
                     Question.Option(id: "True"),
                     Question.Option(id: "False")
@@ -44,35 +44,11 @@ class PromotionContentProvider: ObservableObject {
         
         questions.append(
             Question(
-                text: "Which one of these is the enemy of productivity?",
+                text: "Why is output not as important for promotion?",
                 options: [
-                    Question.Option(id: "JavaScript"),
-                    Question.Option(id: "Time"),
-                    Question.Option(id: "Ambiguity"),
-                    Question.Option(id: "Music")
-                ],
-                correctAnswer: "Ambiguity"
-            )
-        )
-        
-        questions.append(
-            Question(
-                text: "True or false: A lot of senior engineers write very little code, adding value in other ways.",
-                options: [
-                    Question.Option(id: "True"),
-                    Question.Option(id: "False")
-                ],
-                correctAnswer: "True"
-            )
-        )
-        
-        questions.append(
-            Question(
-                text: "Why is it important to break problems down?",
-                options: [
-                    Question.Option(id: "So that your brain registers wins more often"),
-                    Question.Option(id: "To make projects more concrete and easier to understand"),
-                    Question.Option(id: "To allow work to be done in parallel when you have a team"),
+                    Question.Option(id: "It's not physically scalable"),
+                    Question.Option(id: "It's really hard to have a lot of influence just writing code"),
+                    Question.Option(id: "Becoming more senior is more about empowering others to write code"),
                     Question.Option(id: "All of the above")
                 ],
                 correctAnswer: "All of the above"
