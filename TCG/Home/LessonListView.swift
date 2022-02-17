e905f569822aff7bd471bbfcb43fe47f120b92c0
Gear61 <chioualexander@yahoo.com>
2022-02-16 18:36:05 -0800

    Add validation logic to added lessons

--- TCG/Home/LessonListView.swift
+++ TCG/Home/LessonListView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct LessonListView: View {
-
+    
     @EnvironmentObject var interviewLessons: InterviewContentProvider
     @EnvironmentObject var resumeLessons: ResumeContentProvider
     @EnvironmentObject var productivityLessons: ProductivityContentProvider
@@ -17,15 +17,44 @@ struct LessonListView: View {
     var lessonType: LessonType
     
     private func getLessons() -> [Lesson] {
+        var lessons: [Lesson] = []
         switch lessonType {
         case .INTERVIEWING:
-            return interviewLessons.lessons
+            lessons = interviewLessons.lessons
         case .RESUME:
-            return resumeLessons.lessons
+            lessons = resumeLessons.lessons
         case .PRODUCTIVITY:
-            return productivityLessons.lessons
+            lessons = productivityLessons.lessons
         case .PROMOTION:
-            return promotionLessons.lessons
+            lessons = promotionLessons.lessons
+        }
+        verifyLessons(lessons: lessons)
+        return lessons
+    }
+    
+    private func verifyLessons(lessons: [Lesson]) {
+        var seenYouTubeIds = Set<String>()
+        for (index, lesson) in lessons.enumerated() {
+            var lessonIdPrefix = ""
+            switch lessonType {
+            case .INTERVIEWING:
+                lessonIdPrefix = "interviewing"
+            case .RESUME:
+                lessonIdPrefix = "resume"
+            case .PRODUCTIVITY:
+                lessonIdPrefix = "productivity"
+            case .PROMOTION:
+                lessonIdPrefix = "promotion"
+            }
+            let lessonIdNumber = String(index + 1)
+            let expectedLessonId = lessonIdPrefix + "-" + lessonIdNumber
+            if (expectedLessonId != lesson.id) {
+                fatalError("Expected " + expectedLessonId + " and got " + lesson.id)
+            }
+            if (seenYouTubeIds.contains(lesson.youtubeVideoId)) {
+                fatalError("Duplicate YouTube video ID of " + lesson.youtubeVideoId + " || Lesson ID: " + lesson.id)
+            }
+            seenYouTubeIds.insert(lesson.youtubeVideoId)
         }
     }
     
